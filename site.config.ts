376b2d5ffb2c08532b990da64706ec1a993bb241
HyeonSik <whgustlr0625@gmail.com>
2025-09-29 13:41:36 +0900

    2차 수정

--- site.config.ts
+++ site.config.ts
@@ -10,15 +10,15 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'hyeon.s2',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it.tt',
-  author: 'Travis Fischer',
+  domain: 'chobazzi.vercel.app/',
+  author: 'HyeonSik Cho',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'hyeonsik blog',
 
   // social usernames (optional)
   //twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'ChoBazzi',
   //linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+   navigationStyle: 'custom',
+   navigationLinks: [
+     {
+       title: 'About',
+       pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+     },
+     {
+       title: 'Contact',
+       pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+     }
+   ]
 })
